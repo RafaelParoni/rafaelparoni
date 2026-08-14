b2500207389111a5a67e97a12a9f371b44f1c2b1
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2026-08-14 11:58:49 -0300

    Atualizando o css de todas as paginas para deixar um css mestre "main.css"

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './main.css'
 import App from './pages/Home'
 import ParoniDownloader from './pages/ParoniDownloader'
 import QrCustom from './pages/QrCustom'
